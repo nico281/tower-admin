d042b79de77092f96dffcdb34ae33a40c113f8c5
nico <nico281@hotmail.com>
2025-08-27 01:43:03 -0300

    add user cration validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,18 @@ class User < ApplicationRecord
   belongs_to :company, optional: true
   enum :role, [ :super_admin, :admin, :manager, :accountant, :resident ]
 
+  # Validation to ensure super_admin users don't have a company
+  validate :super_admin_cannot_have_company
+
   def super_admin?
     super_admin
+  end
+
+  private
+
+  def super_admin_cannot_have_company
+    if super_admin? && company_id.present?
+      errors.add(:company_id, "cannot be selected for super admin users")
+    end
   end
 end
